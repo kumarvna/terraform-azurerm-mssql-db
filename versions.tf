128c559ee549abcb07d467c6edbfba1f765fb2e8
Kumaraswamy Vithanala <36370570+kumarvna@users.noreply.github.com>
2021-06-05 15:43:54 +0530

    module update

---
+++ versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.59.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "azurerm" {
+  features {}
+}
